45bf4c47466df430371fe9f9aa704222d1dcbeb6
maskedmarker <changjianghui0822@gmail.com>
2025-01-21 21:02:31 +0800

    lambda

--- ch02-lambda/src/test/java/org/example/learn/java/spec/lambda/MethodReferenceTest.java
+++ ch02-lambda/src/test/java/org/example/learn/java/spec/lambda/MethodReferenceTest.java
@@ -7,34 +7,32 @@ import java.util.function.Function;
 
 public class MethodReferenceTest {
 
-    static <R> R playOneArgument(String s1, Function<String, R> func) {
-        return func.apply(s1);
+    static <R> R playOneArgument(String param, Function<String, R> func) {
+        return func.apply(param);
     }
 
-    static Boolean playTwoArgument(String s1, String s2, BiPredicate<String, String> func) {
-        return func.test(s1, s2);
+    static Boolean playTwoArgument(String param1, String param2, BiPredicate<String, String> func) {
+        return func.test(param1, param2);
     }
 
     /**
+     * 使用lambda替代method reference,做等价转换
      * 注意参数个数
      */
     @Test
     public void test0() {
         // lambda
         int result = playOneArgument("mkyong", x -> x.length());   // 6
-
         // method reference
         int result2 = playOneArgument("mkyong", String::length);   // 6
 
         // lambda
         Boolean result3 = playTwoArgument("mkyong", "y", (a, b) -> a.contains(b)); // true
-
         // method reference
         Boolean result4 = playTwoArgument("mkyong", "y", String::contains);        // true
 
         // lambda
         Boolean result5 = playTwoArgument("mkyong", "1", (a, b) -> a.startsWith(b)); // false
-
         // method reference
         Boolean result6 = playTwoArgument("mkyong", "y", String::startsWith);        // false
 
