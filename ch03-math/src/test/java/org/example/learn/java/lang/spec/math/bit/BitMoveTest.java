68c5e394806f1a746cbdb65cf6f694e2da67da96
maskedmarker <changjianghui0822@gmail.com>
2025-11-08 09:38:28 +0800

    add arithmetic

--- ch03-math/src/test/java/org/example/learn/java/lang/spec/math/bit/BitMoveTest.java
+++ ch03-math/src/test/java/org/example/learn/java/lang/spec/math/bit/BitMoveTest.java
@@ -31,7 +31,11 @@ public class BitMoveTest {
         System.out.println("Integer.toBinaryString((b7 << 8)) = " + Integer.toBinaryString((b7 << 8)));
 
         // Shift operation '<<' by overly large constant value 32
+        System.out.println("Integer.toBinaryString(b1 << 31) = " + Integer.toBinaryString(b1 << 31));
         System.out.println("Integer.toBinaryString((b1 << 32)) = " + Integer.toBinaryString((b1 << 32)));
+        System.out.println("Integer.toBinaryString((b1 << 32)) = " + Integer.toBinaryString((b1 << 33)));
+        Assert.assertEquals("shift运算符的右操作数时,等价于先让右操作数mod 32", (b1 << 0), (b1 << (32%32)));
+        Assert.assertEquals("shift运算符的右操作数时,等价于先让右操作数mod 32", (b1 << 1), (b1 << (33%32)));
         System.out.println("Integer.toBinaryString((b1 << 32)) = " + Integer.toBinaryString(((b1 << 31) << 3)));
     }
 
@@ -44,5 +48,36 @@ public class BitMoveTest {
         BigInteger bigInteger = BigInteger.ONE.shiftLeft(100);
         System.out.println("(1 << 100) = " + bigInteger);
         Assert.assertEquals(new BigInteger("1267650600228229401496703205376"), BigInteger.ONE.shiftLeft(100));
+    }
+
+    @Test
+    public void test2() {
+        final int countBits = Integer.SIZE - 3;
+        System.out.println("countBits = " + countBits);
+
+        int running = -1 << countBits; // -1的补码各个位都是1,此时左移多少位,相当于最右多少个位改为0. 此处等价于高三位都为1,其余为0
+        System.out.println("Integer.toBinaryString(running) = " + Integer.toBinaryString(running));
+
+        final int capacity   = (1 << countBits) - 1; // 等价于是最低的countBits个位都是1
+        System.out.println("Integer.toBinaryString(capacity) = " + Integer.toBinaryString(capacity));
+    }
+
+    @Test
+    public void test22() {
+        int COUNT_BITS = Integer.SIZE - 3;
+        int CAPACITY   = (1 << COUNT_BITS) - 1;
+        // runState is stored in the high-order bits
+        int RUNNING    = -1 << COUNT_BITS;
+        int SHUTDOWN   =  0 << COUNT_BITS;
+        int STOP       =  1 << COUNT_BITS;
+        int TIDYING    =  2 << COUNT_BITS;
+        int TERMINATED =  3 << COUNT_BITS;
+
+        System.out.println("Integer.toBinaryString(CAPACITY) = " + Integer.toBinaryString(CAPACITY));
+        System.out.println("Integer.toBinaryString(RUNNING) = " + Integer.toBinaryString(RUNNING)); // 111_00000000000000000000000000000
+        System.out.println("Integer.toBinaryString(SHUTDOWN) = " + Integer.toBinaryString(SHUTDOWN)); // 000_00000000000000000000000000000
+        System.out.println("Integer.toBinaryString(STOP) = " + Integer.toBinaryString(STOP)); // 001_00000000000000000000000000000
+        System.out.println("Integer.toBinaryString(TIDYING) = " + Integer.toBinaryString(TIDYING)); // 010_00000000000000000000000000000
+        System.out.println("Integer.toBinaryString(TERMINATED) = " + Integer.toBinaryString(TERMINATED)); // 011_00000000000000000000000000000
     }
 }
