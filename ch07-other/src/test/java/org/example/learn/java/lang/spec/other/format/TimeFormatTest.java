236a7a32a9c2f47f5af11ba45078e4b4408f7f4f
maskedmarker <changjianghui0822@gmail.com>
2025-11-13 11:37:23 +0800

    date-formatter

--- ch07-other/src/test/java/org/example/learn/java/lang/spec/other/format/TimeFormatTest.java
+++ ch07-other/src/test/java/org/example/learn/java/lang/spec/other/format/TimeFormatTest.java
@@ -2,8 +2,10 @@ package org.example.learn.java.lang.spec.other.format;
 
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Formatter;
 
 public class TimeFormatTest {
 
@@ -11,5 +13,12 @@ public class TimeFormatTest {
     public void defaultFormat() {
         Date now = Calendar.getInstance().getTime();
         System.out.println("now = " + now);
+    }
+
+    @Test
+    public void test01() {
+        Date now = Calendar.getInstance().getTime();
+        String str = new SimpleDateFormat("yyyyMMdd HH:mm:ss:SSS").format(now);
+        System.out.println("str = " + str);
     }
 }
