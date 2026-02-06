2de9c9389c1156393fc8c83077ff799ca2614ba2
maskedmarker <changjianghui0822@gmail.com>
2026-02-06 11:45:16 +0800

    arrayCopy

--- ch07-other/src/test/java/org/example/learn/java/lang/spec/other/collection/ArrayCopyTest.java
+++ ch07-other/src/test/java/org/example/learn/java/lang/spec/other/collection/ArrayCopyTest.java
@@ -6,6 +6,20 @@ import java.util.Arrays;
 
 /**
  * 数组复制
+
+
+ System.arraycopy
+
+ 在Linux 平台上,java.lang.System.arraycopy 最终会落到HotSpot JVM内部的 arraycopy Stub,
+ 对于primitive数组,本质上就是一次高度优化的 memmove/memcpy; 对于 reference 数组,则是带 GC write barrier 的逐元素拷贝.
+ memcpy不是简单地直接调用glibc的memcpy而是：JVM 自己决定用 intrinsic / 汇编 stub / C++ runtime / memmove / 向量化 copy / GC-aware copy 中的哪一种
+
+
+ 对比 Unsafe.copyMemory vs System.arraycopy
+ System.arraycopy 是“语义完整 + GC 安全 + JVM 强保证”的拷贝; Unsafe.copyMemory 是“裸内存搬运指令”,速度潜力更大,但责任完全在调用者.
+ System.arraycopy = JVM托管的memmove; Unsafe.copyMemory =你自己写的memcpy(我只帮你把这段字节挪过去,其他一概不管.)
+ Unsafe.copyMemory的主战场是off-heap/DirectMemory
+
  */
 public class ArrayCopyTest {
 
@@ -24,7 +38,7 @@ public class ArrayCopyTest {
 
     /**
      * System.arraycopy是native方法,对于数组的复制是高效的
-     * src/dest是同一个对象
+     * src/dest是同一个对象 (支持判断是否有内存重叠overlap)
      *
      * If the src and dest arguments refer to the same array object,
      * then the copying is performed as if the components at positions srcPos through srcPos+length-1 were first copied to a temporary array with length components
