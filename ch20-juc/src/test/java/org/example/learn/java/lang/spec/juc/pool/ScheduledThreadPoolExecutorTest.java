e097ed3217732dffc4cc55699ecd09289c05c24b
maskedmarker <changjianghui0822@gmail.com>
2026-04-14 23:13:57 +0800

    add

--- ch20-juc/src/test/java/org/example/learn/java/lang/spec/juc/pool/ScheduledThreadPoolExecutorTest.java
+++ ch20-juc/src/test/java/org/example/learn/java/lang/spec/juc/pool/ScheduledThreadPoolExecutorTest.java
@@ -5,13 +5,29 @@ import org.junit.Test;
 import java.util.Calendar;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 /**
  * 同一个任务不会被并发执行
+ * 任务执行时间大于周期时,下一次任务会在前一次任务结束后立刻开始，整体执行频率低于周期
  */
 public class ScheduledThreadPoolExecutorTest {
+
+    private static class WarmupJob implements Runnable{
+
+        @Override
+        public void run() {
+            System.out.println(Calendar.getInstance().getTime() + Thread.currentThread().getName() + " begin");
+            try {
+                TimeUnit.MILLISECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Calendar.getInstance().getTime() + Thread.currentThread().getName() + " end");
+        }
+    }
 
     private static class MyJob implements Runnable{
 
@@ -44,6 +60,9 @@ public class ScheduledThreadPoolExecutorTest {
     @Test
     public void test01() throws InterruptedException {
         ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(5);
+        scheduledExecutorService.submit(new WarmupJob());
+        scheduledExecutorService.submit(new WarmupJob());
+        scheduledExecutorService.submit(new WarmupJob());
         scheduledExecutorService.scheduleAtFixedRate(new MyJob(), 0, 2, TimeUnit.SECONDS);   // 任务开始的时间是固定的离散点,下一次开始时间 = 上一次开始时间 + period
 
 
@@ -62,6 +81,27 @@ public class ScheduledThreadPoolExecutorTest {
 
 
         while (!scheduledExecutorService.isShutdown()) {
+            Thread.yield();
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+
+    @Test
+    public void test03() throws InterruptedException {
+        ScheduledExecutorService pool = new ScheduledThreadPoolExecutor(2);
+
+        pool.scheduleAtFixedRate(() -> {
+            System.out.println(Thread.currentThread().getName() + " start");
+            try {
+                TimeUnit.SECONDS.sleep(8);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + " end");
+        }, 0, 5, TimeUnit.SECONDS);
+
+
+        while (!pool.isShutdown()) {
             Thread.yield();
             TimeUnit.SECONDS.sleep(1);
         }
